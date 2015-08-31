dcdce98ff4c6e8689d53df230cca03c462d49c80
Uluc Aydin <social@ulucaydin.com>
2015-08-31 14:08:03 -0500

    Update config-default.sh

--- cluster/ubuntu/config-default.sh
+++ cluster/ubuntu/config-default.sh
@@ -18,15 +18,15 @@
 
 # Define all your cluster nodes, MASTER node comes first"
 # And separated with blank space like <user_1@ip_1> <user_2@ip_2> <user_3@ip_3> 
-export nodes=${nodes:-"vcap@10.10.103.250 vcap@10.10.103.162 vcap@10.10.103.223"}
+#export nodes=${nodes:-"vcap@10.10.103.250 vcap@10.10.103.162 vcap@10.10.103.223"}
 # Define all your nodes role: a(master) or i(minion) or ai(both master and minion), must be the order same 
-roles=${roles:-"ai i i"}
+#roles=${roles:-"ai i i"}
 # If it practically impossible to set an array as an environment variable
 # from a script, so assume variable is a string then convert it to an array
 export roles=($roles)
 
 # Define minion numbers
-export NUM_MINIONS=${NUM_MINIONS:-3}
+#export NUM_MINIONS=${NUM_MINIONS:-3}
 # define the IP range used for service cluster IPs.
 # according to rfc 1918 ref: https://tools.ietf.org/html/rfc1918 choose a private ip range here.
 export SERVICE_CLUSTER_IP_RANGE=${SERVICE_CLUSTER_IP_RANGE:-192.168.3.0/24}  # formerly PORTAL_NET
